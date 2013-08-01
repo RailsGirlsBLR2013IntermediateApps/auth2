04233d4f16c5b569703dfccb6771cd5c37e036db
abishekk92 <bhat.abishek@gmail.com>
2013-08-01 16:40:36 +0530

    Initial Commit

---
+++ auth2/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Auth2::Application.config.secret_key_base = '52450521073858f0d6d7098aedf65fafe18eab1864d287598fac10064acff87e1753539e0c48ac08faf9706235b0afedbde08401b372097984b6f731e87b74ea'
